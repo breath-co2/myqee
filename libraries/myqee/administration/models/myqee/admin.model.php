24415d7fc9a28fe96d5d68159aec4c1b8b4dd0b7
呼吸二氧化碳 <jonwang@queyang.com>
2013-01-05 16:55:35 +0800

    model配置文件更新

--- libraries/myqee/administration/models/myqee/admin.model.php
+++ libraries/myqee/administration/models/myqee/admin.model.php
@@ -1,4 +1,13 @@
 <?php
+if (!defined('MODEL_ADMIN_DATABASE'))
+{
+    /**
+     * 定义后台默认数据库配置
+     *
+     * @var string
+     */
+    define('MODEL_ADMIN_DATABASE', Core::config('admin/core.database') );
+}
 
 /**
  * 后台管理基础模块
