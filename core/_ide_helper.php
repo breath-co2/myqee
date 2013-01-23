2a9ccf7a7400d589646e9b3bbcdd6c8b48a77882
呼吸二氧化碳 <jonwang@queyang.com>
2013-01-24 00:11:26 +0800

    add core ide helper

---
+++ core/_ide_helper.php
@@ -0,0 +1,108 @@
+<?php
+exit;
+
+
+class Arr extends Core_Arr{}
+class EX_Arr extends Core_Arr{}
+
+class Auth extends Core_Auth{}
+class EX_Auth extends Core_Auth{}
+
+class Cache extends Core_Cache{}
+class EX_Cache extends Core_Cache{}
+
+class Captcha extends Core_Captcha{}
+class EX_Captcha extends Core_Captcha{}
+
+class Config extends Core_Config{}
+class EX_Config extends Core_Config{}
+
+class Controller extends Core_Controller{}
+class EX_Controller extends Core_Controller{}
+
+class Cookie extends Core_Cookie{}
+class EX_Cookie extends Core_Cookie{}
+
+abstract class Core extends Core_Core{}
+abstract class EX_Core extends Core_Core{}
+
+class Database extends Core_Database{}
+class EX_Database extends Core_Database{}
+
+class Date extends Core_Date{}
+class EX_Date extends Core_Date{}
+
+class Email extends Core_Email{}
+class EX_Email extends Core_Email{}
+
+class File extends Core_File{}
+class EX_File extends Core_File{}
+
+class Form extends Core_Form{}
+class EX_Form extends Core_Form{}
+
+class FTP extends Core_FTP{}
+class EX_FTP extends Core_FTP{}
+
+class HTML extends Core_HTML{}
+class EX_HTML extends Core_HTML{}
+
+class HttpCall extends Core_HttpCall{}
+class EX_HttpCall extends Core_HttpCall{}
+
+class HttpClient extends Core_HttpClient{}
+class EX_HttpClient extends Core_HttpClient{}
+
+class HttpIO extends Core_HttpIO{}
+class EX_HttpIO extends Core_HttpIO{}
+
+abstract class I18n extends Core_I18n{}
+abstract class EX_I18n extends Core_I18n{}
+
+class IpSource extends Core_IpSource{}
+class EX_IpSource extends Core_IpSource{}
+
+class Member extends Core_Member{}
+class EX_Member extends Core_Member{}
+
+class Model extends Core_Model{}
+class EX_Model extends Core_Model{}
+
+class Pagination extends Core_Pagination{}
+class EX_Pagination extends Core_Pagination{}
+
+class Permission extends Core_Permission{}
+class EX_Permission extends Core_Permission{}
+
+class PinYin extends Core_PinYin{}
+class EX_PinYin extends Core_PinYin{}
+
+class Route extends Core_Route{}
+class EX_Route extends Core_Route{}
+
+class Session extends Core_Session{}
+class EX_Session extends Core_Session{}
+
+class Str extends Core_Str{}
+class EX_Str extends Core_Str{}
+
+class Text extends Core_Text{}
+class EX_Text extends Core_Text{}
+
+class Upload extends Core_Upload{}
+class EX_Upload extends Core_Upload{}
+
+class UTF8 extends Core_UTF8{}
+class EX_UTF8 extends Core_UTF8{}
+
+class Valid extends Core_Valid{}
+class EX_Valid extends Core_Valid{}
+
+class View extends Core_View{}
+class EX_View extends Core_View{}
+
+class Controller_Captcha extends Core_Controller_Captcha{}
+class EX_Controller_Captcha extends Core_Controller_Captcha{}
+
+class Controller_OpenDebugger extends Core_Controller_OpenDebugger{}
+class EX_Controller_OpenDebugger extends Core_Controller_OpenDebugger{}
