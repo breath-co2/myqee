957884f64edad04a6f06259908b67ac15f7e0c0c
呼吸二氧化碳 <jonwang@queyang.com>
2013-01-26 15:29:16 +0800

    Core IDE Helper更新

--- core/_ide_helper.php
+++ core/_ide_helper.php
@@ -8,6 +8,27 @@ class EX_Arr extends Core_Arr{}
 class Auth extends Core_Auth{}
 class EX_Auth extends Core_Auth{}
 
+class Cache_Driver_Apc extends Core_Cache_Driver_Apc{}
+class EX_Cache_Driver_Apc extends Core_Cache_Driver_Apc{}
+
+class Cache_Driver_Database extends Core_Cache_Driver_Database{}
+class EX_Cache_Driver_Database extends Core_Cache_Driver_Database{}
+
+class Cache_Driver_File extends Core_Cache_Driver_File{}
+class EX_Cache_Driver_File extends Core_Cache_Driver_File{}
+
+class Cache_Driver_Memcache extends Core_Cache_Driver_Memcache{}
+class EX_Cache_Driver_Memcache extends Core_Cache_Driver_Memcache{}
+
+class Cache_Driver_Redis extends Core_Cache_Driver_Redis{}
+class EX_Cache_Driver_Redis extends Core_Cache_Driver_Redis{}
+
+class Cache_Driver_SQLite extends Core_Cache_Driver_SQLite{}
+class EX_Cache_Driver_SQLite extends Core_Cache_Driver_SQLite{}
+
+class Cache_Driver_WinCache extends Core_Cache_Driver_WinCache{}
+class EX_Cache_Driver_WinCache extends Core_Cache_Driver_WinCache{}
+
 class Cache extends Core_Cache{}
 class EX_Cache extends Core_Cache{}
 
@@ -25,6 +46,51 @@ class EX_Cookie extends Core_Cookie{}
 
 abstract class Core extends Core_Core{}
 abstract class EX_Core extends Core_Core{}
+
+class Database_Driver_Mongo_Result extends Core_Database_Driver_Mongo_Result{}
+class EX_Database_Driver_Mongo_Result extends Core_Database_Driver_Mongo_Result{}
+
+class Database_Driver_Mongo extends Core_Database_Driver_Mongo{}
+class EX_Database_Driver_Mongo extends Core_Database_Driver_Mongo{}
+
+class Database_Driver_MySQL_Result extends Core_Database_Driver_MySQL_Result{}
+class EX_Database_Driver_MySQL_Result extends Core_Database_Driver_MySQL_Result{}
+
+abstract class Database_Driver_MySQL_Transaction extends Core_Database_Driver_MySQL_Transaction{}
+abstract class EX_Database_Driver_MySQL_Transaction extends Core_Database_Driver_MySQL_Transaction{}
+
+class Database_Driver_MySQL extends Core_Database_Driver_MySQL{}
+class EX_Database_Driver_MySQL extends Core_Database_Driver_MySQL{}
+
+class Database_Driver_MySQLI_Result extends Core_Database_Driver_MySQLI_Result{}
+class EX_Database_Driver_MySQLI_Result extends Core_Database_Driver_MySQLI_Result{}
+
+abstract class Database_Driver_MySQLI_Transaction extends Core_Database_Driver_MySQLI_Transaction{}
+abstract class EX_Database_Driver_MySQLI_Transaction extends Core_Database_Driver_MySQLI_Transaction{}
+
+class Database_Driver_MySQLI extends Core_Database_Driver_MySQLI{}
+class EX_Database_Driver_MySQLI extends Core_Database_Driver_MySQLI{}
+
+class Database_Driver_SQLite_Result extends Core_Database_Driver_SQLite_Result{}
+class EX_Database_Driver_SQLite_Result extends Core_Database_Driver_SQLite_Result{}
+
+class Database_Driver_SQLite extends Core_Database_Driver_SQLite{}
+class EX_Database_Driver_SQLite extends Core_Database_Driver_SQLite{}
+
+abstract class Database_Driver extends Core_Database_Driver{}
+abstract class EX_Database_Driver extends Core_Database_Driver{}
+
+class Database_Expression extends Core_Database_Expression{}
+class EX_Database_Expression extends Core_Database_Expression{}
+
+class Database_QueryBuilder extends Core_Database_QueryBuilder{}
+class EX_Database_QueryBuilder extends Core_Database_QueryBuilder{}
+
+abstract class Database_Result extends Core_Database_Result{}
+abstract class EX_Database_Result extends Core_Database_Result{}
+
+abstract class Database_Transaction extends Core_Database_Transaction{}
+abstract class EX_Database_Transaction extends Core_Database_Transaction{}
 
 class Database extends Core_Database{}
 class EX_Database extends Core_Database{}
@@ -50,6 +116,15 @@ class EX_HTML extends Core_HTML{}
 class HttpCall extends Core_HttpCall{}
 class EX_HttpCall extends Core_HttpCall{}
 
+class HttpClient_Driver_Curl extends Core_HttpClient_Driver_Curl{}
+class EX_HttpClient_Driver_Curl extends Core_HttpClient_Driver_Curl{}
+
+class HttpClient_Driver_Fsock extends Core_HttpClient_Driver_Fsock{}
+class EX_HttpClient_Driver_Fsock extends Core_HttpClient_Driver_Fsock{}
+
+class HttpClient_Result extends Core_HttpClient_Result{}
+class EX_HttpClient_Result extends Core_HttpClient_Result{}
+
 class HttpClient extends Core_HttpClient{}
 class EX_HttpClient extends Core_HttpClient{}
 
@@ -68,6 +143,27 @@ class EX_Member extends Core_Member{}
 class Model extends Core_Model{}
 class EX_Model extends Core_Model{}
 
+class OOP_ORM_Data extends Core_OOP_ORM_Data{}
+class EX_OOP_ORM_Data extends Core_OOP_ORM_Data{}
+
+class OOP_ORM_Finder_DB extends Core_OOP_ORM_Finder_DB{}
+class EX_OOP_ORM_Finder_DB extends Core_OOP_ORM_Finder_DB{}
+
+class OOP_ORM_Finder_REST extends Core_OOP_ORM_Finder_REST{}
+class EX_OOP_ORM_Finder_REST extends Core_OOP_ORM_Finder_REST{}
+
+abstract class OOP_ORM_Index extends Core_OOP_ORM_Index{}
+abstract class EX_OOP_ORM_Index extends Core_OOP_ORM_Index{}
+
+abstract class OOP_ORM_Parse extends Core_OOP_ORM_Parse{}
+abstract class EX_OOP_ORM_Parse extends Core_OOP_ORM_Parse{}
+
+class OOP_ORM_Result extends Core_OOP_ORM_Result{}
+class EX_OOP_ORM_Result extends Core_OOP_ORM_Result{}
+
+abstract class OOP_ORM extends Core_OOP_ORM{}
+abstract class EX_OOP_ORM extends Core_OOP_ORM{}
+
 class Pagination extends Core_Pagination{}
 class EX_Pagination extends Core_Pagination{}
 
@@ -79,6 +175,12 @@ class EX_PinYin extends Core_PinYin{}
 
 class Route extends Core_Route{}
 class EX_Route extends Core_Route{}
+
+class Session_Driver_Cache extends Core_Session_Driver_Cache{}
+class EX_Session_Driver_Cache extends Core_Session_Driver_Cache{}
+
+class Session_Driver_Default extends Core_Session_Driver_Default{}
+class EX_Session_Driver_Default extends Core_Session_Driver_Default{}
 
 class Session extends Core_Session{}
 class EX_Session extends Core_Session{}
@@ -100,6 +202,18 @@ class EX_Valid extends Core_Valid{}
 
 class View extends Core_View{}
 class EX_View extends Core_View{}
+
+class Controller_Index extends Core_Controller_Index{}
+class EX_Controller_Index extends Core_Controller_Index{}
+
+class Controller_RunTime extends Core_Controller_RunTime{}
+class EX_Controller_RunTime extends Core_Controller_RunTime{}
+
+abstract class Controller_Shell extends Core_Controller_Shell{}
+abstract class EX_Controller_Shell extends Core_Controller_Shell{}
+
+class Controller_File extends Core_Controller_File{}
+class EX_Controller_File extends Core_Controller_File{}
 
 class Controller_Captcha extends Core_Controller_Captcha{}
 class EX_Controller_Captcha extends Core_Controller_Captcha{}
