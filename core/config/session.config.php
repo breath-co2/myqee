7171cd936127d1533616e24af7378d10bd8e70be
呼吸二氧化碳 <jonwang@queyang.com>
2013-01-26 15:47:10 +0800

    Core配置文件统一到一个文件里

--- core/config/session.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = array
-(
-	'name'           => 'SID',    // SessionID名称
-	'expiration'     => 0,
-	'gc_probability' => 0,
-    'check_string'   => '$@de23#$%@.dG2.p4Ad',    // 校验字符串
-);
