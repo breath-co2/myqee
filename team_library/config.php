04531685caeb672edabb5b91f42388fb7f1b9c8b
呼吸二氧化碳 <jonwang@queyang.com>
2013-02-05 22:23:57 +0800

    config增加注释

--- team_library/config.php
+++ team_library/config.php
@@ -1,11 +1,6 @@
 <?php
+// 这里可写下全项目同意的配置，比如 $config['database'] 等
 
-/**
- * 数据库配置
- *
- * @var array
- */
-$database = array
-(
+// 在程序中可通过 Core::config('mytest') 获取 $config['mytest'] 的值
+// $config['mytest'] = 1;
 
-);
