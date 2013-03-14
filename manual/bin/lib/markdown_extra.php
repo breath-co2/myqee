492f101dd1abc986b4edaafe5c7eeaacaa825bf5
呼吸二氧化碳 <jonwang@queyang.com>
2013-03-15 00:05:12 +0800

    手册功能更新

---
+++ manual/bin/lib/markdown_extra.php
@@ -0,0 +1,40 @@
+<?php
+
+class MarkdownExtra extends _MarkdownExtra_TmpImpl
+{
+    function __construct()
+    {
+        $this->block_gamut += array
+        (
+            'doHeaders2' => 11,
+        );
+
+        parent::__construct();
+    }
+
+
+    function doHeaders2($text) {
+        # atx-style headers:
+        #   !!! Header 1
+        #   !!! Header 2 with closing hashes !!!
+        #   ...
+        #   ###### Header 6
+        #
+        $text = preg_replace_callback('{
+                ^(\!{1,6})  # $1 = string of !\'s
+                [ ]*
+                (.+?)       # $2 = Header text
+                [ ]*
+                \!*         # optional closing #\'s (not counted)
+                \n+
+            }xm',
+            array(&$this, '_doHeaders2_callback_atx'), $text);
+
+        return $text;
+    }
+
+    function _doHeaders2_callback_atx($matches) {
+        $block = '<div class="alert alert-error">'.$this->runSpanGamut($matches[2])."</div>";
+        return "\n" . $this->hashBlock($block) . "\n\n";
+    }
+}
