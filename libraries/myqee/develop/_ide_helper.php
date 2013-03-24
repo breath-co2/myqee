8f6c1296fb72573bdc5e9800e776d2d7fe776eb8
呼吸二氧化碳 <jonwang@queyang.com>
2013-03-24 21:31:37 +0800

    价格develop类库的ide_helper文件

---
+++ libraries/myqee/develop/_ide_helper.php
@@ -0,0 +1,15 @@
+<?php
+exit;
+
+
+class Debug extends Library_MyQEE_Develop_Debug{}
+class EX_Debug extends Library_MyQEE_Develop_Debug{}
+
+class ErrException extends Library_MyQEE_Develop_ErrException{}
+class EX_ErrException extends Library_MyQEE_Develop_ErrException{}
+
+class Profiler extends Library_MyQEE_Develop_Profiler{}
+class EX_Profiler extends Library_MyQEE_Develop_Profiler{}
+
+class Controller_Assets extends Library_MyQEE_Develop_Controller_Assets{}
+class EX_Controller_Assets extends Library_MyQEE_Develop_Controller_Assets{}
