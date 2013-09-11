9cf73d12e41bc20f463a6cee0af98f5947d4650e
呼吸二氧化碳 <jonwang@queyang.com>
2013-09-11 21:31:53 +0800

    recreat-idb-helper-file 脚本修复bug

--- core/_ide_helper.php
+++ core/_ide_helper.php
@@ -5,14 +5,92 @@ exit;
 class Arr extends Core_Arr{}
 class Ex_Arr extends Core_Arr{}
 
+class Auth extends Core_Auth{}
+class Ex_Auth extends Core_Auth{}
+
+class Captcha extends Core_Captcha{}
+class Ex_Captcha extends Core_Captcha{}
+
+class Config extends Core_Config{}
+class Ex_Config extends Core_Config{}
+
+class Controller extends Core_Controller{}
+class Ex_Controller extends Core_Controller{}
+
+class Cookie extends Core_Cookie{}
+class Ex_Cookie extends Core_Cookie{}
+
 abstract class Core extends Core_Core{}
 abstract class Ex_Core extends Core_Core{}
+
+class Date extends Core_Date{}
+class Ex_Date extends Core_Date{}
 
 class Email extends Core_Email{}
 class Ex_Email extends Core_Email{}
 
+class File extends Core_File{}
+class Ex_File extends Core_File{}
+
+class Form extends Core_Form{}
+class Ex_Form extends Core_Form{}
+
+class FTP extends Core_FTP{}
+class Ex_FTP extends Core_FTP{}
+
+class HTML extends Core_HTML{}
+class Ex_HTML extends Core_HTML{}
+
+class HttpCall extends Core_HttpCall{}
+class Ex_HttpCall extends Core_HttpCall{}
+
+abstract class HttpIO extends Core_HttpIO{}
+abstract class Ex_HttpIO extends Core_HttpIO{}
+
+abstract class I18n extends Core_I18n{}
+abstract class Ex_I18n extends Core_I18n{}
+
+abstract class IpSource extends Core_IpSource{}
+abstract class Ex_IpSource extends Core_IpSource{}
+
 class Member extends Core_Member{}
 class Ex_Member extends Core_Member{}
+
+class Model extends Core_Model{}
+class Ex_Model extends Core_Model{}
+
+class Pagination extends Core_Pagination{}
+class Ex_Pagination extends Core_Pagination{}
+
+class Permission extends Core_Permission{}
+class Ex_Permission extends Core_Permission{}
+
+class PinYin extends Core_PinYin{}
+class Ex_PinYin extends Core_PinYin{}
+
+class Route extends Core_Route{}
+class Ex_Route extends Core_Route{}
+
+class Str extends Core_Str{}
+class Ex_Str extends Core_Str{}
+
+abstract class Text extends Core_Text{}
+abstract class Ex_Text extends Core_Text{}
+
+class Upload_Driver_Storage extends Core_Upload_Driver_Storage{}
+class Ex_Upload_Driver_Storage extends Core_Upload_Driver_Storage{}
+
+class Upload extends Core_Upload{}
+class Ex_Upload extends Core_Upload{}
+
+class UTF8 extends Core_UTF8{}
+class Ex_UTF8 extends Core_UTF8{}
+
+class Valid extends Core_Valid{}
+class Ex_Valid extends Core_Valid{}
+
+class View extends Core_View{}
+class Ex_View extends Core_View{}
 
 class ORM_Test_Finder extends Core_ORM_Test_Finder{}
 class Ex_ORM_Test_Finder extends Core_ORM_Test_Finder{}
