c18ae29be010c20646acf97b1eaeb33f5a9957c5
呼吸二氧化碳 <jonwang@queyang.com>
2013-09-11 21:29:19 +0800

    ide 提示辅助文件更新， recreat-idb-helper-file 脚本更新，支持module

---
+++ _ide_helper.php
@@ -0,0 +1,21 @@
+<?php
+exit;
+
+
+class Storage_Driver_Database extends Module_Storage_Driver_Database{}
+class Ex_Storage_Driver_Database extends Module_Storage_Driver_Database{}
+
+class Storage_Driver_File extends Module_Storage_Driver_File{}
+class Ex_Storage_Driver_File extends Module_Storage_Driver_File{}
+
+class Storage_Driver_Redis extends Module_Storage_Driver_Redis{}
+class Ex_Storage_Driver_Redis extends Module_Storage_Driver_Redis{}
+
+class Storage_Driver_Swift extends Module_Storage_Driver_Swift{}
+class Ex_Storage_Driver_Swift extends Module_Storage_Driver_Swift{}
+
+abstract class Storage_Driver extends Module_Storage_Driver{}
+abstract class Ex_Storage_Driver extends Module_Storage_Driver{}
+
+class Storage extends Module_Storage{}
+class Ex_Storage extends Module_Storage{}
