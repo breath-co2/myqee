421274729d33771bce06c3edf33de9e0814131fc
呼吸二氧化碳 <jonwang@queyang.com>
2013-09-11 20:34:16 +0800

    OOP_ORM 独立为模块

--- orm.class.php
+++ orm.class.php
@@ -4,13 +4,12 @@
  * MyQEE ORM 核心类
  *
  * @author     呼吸二氧化碳 <jonwang@myqee.com>
- * @category   MyQEE
- * @package    System
- * @subpackage Core
+ * @category   Module
+ * @package    ORM
  * @copyright  Copyright (c) 2008-2013 myqee.com
  * @license    http://www.myqee.com/license.html
  */
-abstract class Core_OOP_ORM
+abstract class Module_OOP_ORM
 {
 
     /**
