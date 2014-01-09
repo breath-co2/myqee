1dd233474b671a02bd380a6d5ea9d6cbb6c07ae1
呼吸二氧化碳 <jonwang@queyang.com>
2014-01-09 23:54:34 +0800

    HttpClient增加upload方法，可实现上传文件功能

--- modules/httpclient/driver/fsock.class.php
+++ modules/httpclient/driver/fsock.class.php
@@ -22,6 +22,8 @@ class Module_HttpClient_Driver_Fsock
 
     protected $header = array();
 
+    protected $files = array();
+
     protected $_option = array();
 
     protected $_post_data = array();
@@ -132,6 +134,19 @@ class Module_HttpClient_Driver_Fsock
         return $this;
     }
 
+    /**
+     * 添加上次文件
+     *
+     * @param $file_name string 文件路径
+     * @param $name string 文件名
+     * @return $this
+     */
+    public function add_file($file_name, $name)
+    {
+        $this->files[$name] = $file_name;
+
+        return $this;
+    }
 
     /**
      * 设置，获取REST的类型
@@ -178,29 +193,37 @@ class Module_HttpClient_Driver_Fsock
         # POST模式
         $this->method('POST');
 
-        if (is_array($url))
+        if (is_string($url))
         {
-            $myvars = array();
-            foreach ($url as $k=>$url)
+            $vars = array
+            (
+                $vars
+            );
+        }
+        $my_vars = array();
+        foreach ((array)$url as $k=>$u)
+        {
+            if (isset($vars[$k]))
             {
-                if (isset($vars[$k]))
+                if (is_array($vars[$k]))
                 {
-                    if (is_array($vars[$k]))
+                    if ($this->files)
                     {
-                        $myvars[$url] = http_build_query($vars[$k]);
+                        # 如果需要上传文件，则不需要预先将数组转换成字符串
+                        $my_vars[$u] = $vars[$k];
                     }
                     else
                     {
-                        $myvars[$url] = $vars[$k];
+                        $my_vars[$u] = http_build_query($vars[$k]);
                     }
+                }
+                else
+                {
+                    $my_vars[$u] = (string)$vars[$k];
                 }
             }
         }
-        else
-        {
-            $myvars = array($url=>$vars);
-        }
-        $this->_post_data = $myvars;
+        $this->_post_data = $my_vars;
 
         return $this->get($url, $timeout);
     }
@@ -214,14 +237,14 @@ class Module_HttpClient_Driver_Fsock
      */
     public function get($url, $timeout = 10)
     {
-        if ( is_array($url) )
+        if (is_array($url))
         {
-            $getone = false;
+            $get_one = false;
             $urls = $url;
         }
         else
         {
-            $getone = true;
+            $get_one = true;
             $urls = array($url);
         }
 
@@ -229,7 +252,7 @@ class Module_HttpClient_Driver_Fsock
 
         $this->clear_set();
 
-        if ( $getone )
+        if ($get_one)
         {
             $this->http_data = $this->http_data[$url];
             return $data[$url];
@@ -252,29 +275,30 @@ class Module_HttpClient_Driver_Fsock
     {
         $this->method('PUT');
 
-        if (is_array($url))
+        if (is_string($url))
         {
-            $myvars = array();
-            foreach ($url as $k=>$url)
+            $vars = array
+            (
+                $vars
+            );
+        }
+        $my_vars = array();
+        foreach ((array)$url as $k=>$u)
+        {
+            if (isset($vars[$k]))
             {
-                if (isset($vars[$k]))
+                if (is_array($vars[$k]))
                 {
-                    if (is_array($vars[$k]))
-                    {
-                        $myvars[$url] = http_build_query($vars[$k]);
-                    }
-                    else
-                    {
-                        $myvars[$url] = $vars[$k];
-                    }
+                    $my_vars[$u] = http_build_query($vars[$k]);
+                }
+                else
+                {
+                    $my_vars[$u] = (string)$vars[$k];
                 }
             }
         }
-        else
-        {
-            $myvars = array($url=>$vars);
-        }
-        $this->_post_data = $myvars;
+
+        $this->_post_data = $my_vars;
 
         return $this->get($url, $timeout);
     }
@@ -301,9 +325,9 @@ class Module_HttpClient_Driver_Fsock
      *
      * @param string $url URL地址
      * @param int $timeout 超时时间
-     * @return fsockopen()
+     * @return resource fsockopen returns a file pointer which may be used
      */
-    protected function _create($url,$timeout)
+    protected function _create($url, $timeout)
     {
         if (false===strpos($url, '://'))
         {
@@ -384,28 +408,72 @@ class Module_HttpClient_Driver_Fsock
             }
         }
 
-        # 设置POST数据
-        if ($this->_post_data)
+
+        if ($this->files)
         {
-            $vars = (string)$this->_post_data[$the_url];
-            $header['Content-Length'] = strlen($vars);
+            $boundary = '----------------------------' . substr(md5(microtime(1).mt_rand()), 0, 12);
+            $vars = "--$boundary\r\n";
+            if ($this->_post_data[$url])
+            {
+                if (!is_array($this->_post_data[$url]))
+                {
+                    parse_str($this->_post_data[$url], $post);
+                }
+                else
+                {
+                    $post = $this->_post_data[$url];
+                }
+                // form data
+                foreach($post as $key => $val)
+                {
+                    $vars .= "Content-Disposition: form-data; name=\"". rawurlencode($key) ."\"\r\n";
+                    $vars .= "Content-type:application/x-www-form-urlencoded\r\n\r\n";
+                    $vars .= rawurlencode($val) ."\r\n";
+                    $vars .= "--$boundary\r\n";
+                }
+            }
+
+            foreach($this->files as $name=>$filename)
+            {
+                $vars .= "Content-Disposition: form-data; name=\"".$name."\"; filename=\"". rawurlencode(basename($filename)) ."\"\r\n";
+                $vars .= "Content-Type: ".File::mime($filename) ."\r\n\r\n";
+                $vars .= file_get_contents($filename) ."\r\n";
+                $vars .= "--$boundary\r\n";
+            }
+
+            $vars .= "--\r\n\r\n";
+
+            $header['Content-Type']   = 'multipart/form-data; boundary='. $boundary;
+        }
+        else if (isset($this->_post_data[$url]) && $this->_post_data[$url])
+        {
+            # 设置POST数据
+            $vars = is_array($this->_post_data[$url])?http_build_query($this->_post_data[$url]):(string)$this->_post_data[$url];
             $header['Content-Type']   = 'application/x-www-form-urlencoded';
         }
+        else
+        {
+            $vars = '';
+        }
+
+        # 设置长度
+        $header['Content-Length'] = strlen($vars);
 
         $str = $this->method . ' ' . $uri . ' HTTP/1.1'."\r\n";
         foreach ($header as $k=>$v)
         {
-            $str .= $k .' :' . str_replace(array("\r","\n"),'',$v) . "\r\n";
+            $str .= $k .' :' . str_replace(array("\r","\n"), '', $v) . "\r\n";
         }
         $str .= "\r\n";
 
-        if ($this->_post_data)
-        {
-            // 追加POST数据
-            $str .= $vars;
-        }
+        # 写入头信息
+        fwrite($ch, $str);
 
-        fwrite($ch,$str);
+        if ($vars)
+        {
+            # 追加POST数据
+            fwrite($ch, $vars);
+        }
 
         return $ch;
     }
@@ -439,9 +507,9 @@ class Module_HttpClient_Driver_Fsock
 
         # 排队列表
         $multi_list = array();
-        foreach ( $urls as $url )
+        foreach ($urls as $url)
         {
-            if ( $this->multi_exec_num>0 && $list_num>=$this->multi_exec_num )
+            if ($this->multi_exec_num>0 && $list_num>=$this->multi_exec_num)
             {
                 # 加入排队列表
                 $multi_list[] = $url;
@@ -449,7 +517,7 @@ class Module_HttpClient_Driver_Fsock
             else
             {
                 # 列队数控制
-                $listener_list[] = array($url,$this->_create($url, $timeout));
+                $listener_list[] = array($url, $this->_create($url, $timeout));
                 $list_num++;
             }
 
@@ -488,14 +556,14 @@ class Module_HttpClient_Driver_Fsock
                 $code = 0;
             }
 
-            if( strpos($header, 'Transfer-Encoding: chunked') )
+            if(strpos($header, 'Transfer-Encoding: chunked'))
             {
                 $body = explode("\r\n", $body);
                 $body = array_slice($body, 1, -1);
                 $body = implode('', $body);
             }
 
-            if ( preg_match('#Location(?:[ ]*):([^\r]+)\r\n#Uis', $header , $m) )
+            if (preg_match('#Location(?:[ ]*):([^\r]+)\r\n#Uis', $header , $m))
             {
                 if (count($redirect_list[$done_url])>=10)
                 {
@@ -568,7 +636,7 @@ class Module_HttpClient_Driver_Fsock
         return $result;
     }
 
-    public function get_resut_data()
+    public function get_result_data()
     {
         return $this->http_data;
     }
@@ -580,10 +648,11 @@ class Module_HttpClient_Driver_Fsock
     {
         $this->_option    = array();
         $this->header     = array();
+        $this->_post_data = array();
+        $this->files      = array();
         $this->ip         = null;
         $this->cookies    = null;
         $this->referer    = null;
-        $this->_post_data = array();
         $this->method     = 'GET';
     }
 }
